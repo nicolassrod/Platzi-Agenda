b4c46b0bd274d146ee7410dc144ee8e2f3d885da
Nicolás A. Rodríguez <nrq5510@gmail.com>
2019-12-03 00:39:16 -0500

    Initial Commit

---
+++ Platzi Agenda/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Platzi Agenda
+//
+//  Created by Nicolás A. Rodríguez on 12/3/19.
+//  Copyright © 2019 Nicolás A. Rodríguez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
